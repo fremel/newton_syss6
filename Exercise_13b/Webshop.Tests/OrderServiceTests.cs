7ec32744b087c2f241de1a80829a1ebcb771ce35
Fredrik Mellgren <fredrik.mellgren@regent.se>
2024-04-01 12:51:49 +0200

    Implemented test case PlaceOrder_ShouldSendEmail.

--- Exercise_13b/Webshop.Tests/OrderServiceTests.cs
+++ Exercise_13b/Webshop.Tests/OrderServiceTests.cs
@@ -6,6 +6,21 @@ public class OrderServiceTests
     [TestMethod]
     public void PlaceOrder_ShouldSendEmail()
     {
-        
+        // Arrange
+        var mock = new Mock<IEmailService>();
+        OrderService service = new OrderService(mock.Object);
+        OrderDetails orderDetails = new OrderDetails {
+            OrderId = 666,
+            CustomerEmail = "doom.slayer@hotmail.com"
+        };
+
+        // Act
+        service.PlaceOrder(orderDetails);
+
+        // Assert
+        mock.Verify(es => es.SendEmail(
+            orderDetails.CustomerEmail, 
+            "Order Confirmation", 
+            $"Thank you for your order! Order ID: {orderDetails.OrderId}"));
     }
 }
